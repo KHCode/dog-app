7e6caf7be4fd378d044845b74de720a615b4f7e3
KHCode <hillkri@oregonstate.edu>
2021-04-28 06:11:32 -0700

    add axios api call + map data to select menu

--- dog-app/src/components/app-select-menu/app-select-menu.tsx
+++ dog-app/src/components/app-select-menu/app-select-menu.tsx
@@ -1,4 +1,5 @@
-import { Component, Host, h, Fragment } from '@stencil/core';
+import { Component, h } from '@stencil/core';
+import axios from 'axios';
 
 @Component({
   tag: 'app-select-menu',
@@ -16,13 +17,21 @@ export class AppSelectMenu {
   ];
 
   // breedsList = Object.keys(this.dogBreeds).map(key => ({name: key}));
+  dogData;
+  componentWillLoad() {
+    return axios.get('https://dog.ceo/api/breeds/list/all')
+      .then(response => {
+        console.log(Object.keys(response.data.message));
+        this.dogData = Object.keys(response.data.message);
+      });
+  }
 
   render() {
     return (
       <div>
         <label htmlFor="dog-select">Select a Dog Breed</label>
         <select>
-          {this.dogBreeds.map((breed) => (
+          {this.dogData.map((breed) => (
             <option value={breed}>
               {breed}
             </option>
