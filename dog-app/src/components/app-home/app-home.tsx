20f47b5ee81906ed6cd00aad04d3f21750d43c18
KHCode <hillkri@oregonstate.edu>
2021-04-29 15:29:19 -0700

    get app-home to pass props to app-image

--- dog-app/src/components/app-home/app-home.tsx
+++ dog-app/src/components/app-home/app-home.tsx
@@ -1,4 +1,5 @@
-import { Component, h } from '@stencil/core';
+import { Component, h, State, Listen } from '@stencil/core';
+import axios from 'axios';
 
 @Component({
   tag: 'app-home',
@@ -6,11 +7,26 @@ import { Component, h } from '@stencil/core';
   shadow: true,
 })
 export class AppHome {
+  @State() imgUrl: string;
+  @State() dogSelected: string;
+
+  getPicture = (event) => {
+    console.log(event.target.value);
+    this.dogSelected = event.target.value;
+    axios.get(`https://dog.ceo/api/breed/${event.target.value}/images/random`)
+      .then(response => {
+        console.log(response.data.message);
+        this.imgUrl = response.data.message;
+        console.log(this.imgUrl);
+      });
+  }
+
   render() {
     return (
       <div class="app-home">
-        <app-image></app-image>
-        <app-select-menu></app-select-menu>
+        <app-image dogImage={this.imgUrl}></app-image>
+        <app-select-menu handleChange={this.getPicture}></app-select-menu>
+        <app-again-button dogSelected={this.dogSelected}></app-again-button>
       </div>
     );
   }
